56d1812735dd89a9577f0ed6bdce04438f6815cf
Roni Athayde <pontes014@gmail.com>
2023-04-18 10:07:17 -0300

    feat mobile devices

--- assets/js/medium.api.js
+++ assets/js/medium.api.js
@@ -8,7 +8,8 @@ function createTemplateProject({author, link, title, published, category}){
 
     let year = date.getFullYear();
 
-    let formedDate = `${day.toString().padStart(2, 0)}/${month.toString().padStart(2, 0)}/${year}`
+    let formedDate = `${day.toString().padStart(2, 0)}/${month.toString().padStart(2, 0)}/${year}`;
+
 
     let card = `
     <a target="_blank" class="posts_list_link" href="${link}">
@@ -30,7 +31,7 @@ function createTemplateProject({author, link, title, published, category}){
                 
                 <div class="posts_list_project--foot--watch" title="Tags">
                     <i class="fa-solid fa-tags"></i>
-                    ${category}
+                    <span>${category}</span>
                 </div> 
                 
             </footer>
@@ -67,27 +68,4 @@ export  const createConectionMedium = async function(){
         document.querySelector('.posts_list').textContent = e.message
         
     }
-    
-    document.querySelector('.posts--btn').addEventListener('click', function(){// toggle display block/none posts
-
-        if (document.querySelector('.posts_list').dataset.displayPosts == 'hidden'){
-            document.querySelector('.posts--btn').textContent = "Ver menos"
-            console.log('aparece')
-            document.querySelectorAll('.posts_list .posts_list_link:nth-child(n+5):nth-child(-n+200)').forEach( e =>{
-                e.style.display = "block" 
-
-            })
-            document.querySelector('.posts_list').setAttribute('data-display-posts', 'visible')
-
-        }else if (document.querySelector('.posts_list').dataset.displayPosts == 'visible'){
-            document.querySelector('.posts--btn').textContent = "Ver mais"
-
-            document.querySelectorAll('.posts_list .posts_list_link:nth-child(n+5):nth-child(-n+200)').forEach( e =>{
-                e.style.display = "none" 
-
-                
-            })
-            document.querySelector('.posts_list').setAttribute('data-display-posts','hidden')
-        } 
-    })
 }
